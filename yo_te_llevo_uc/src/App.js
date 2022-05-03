c1c18fe12039cf8767cf00b26574ca82fa90d96a
afalarconm <afalarconm@uc.cl>
2022-05-03 12:13:19 -0400

    added Tailwinds :white_check_mark:

--- yo_te_llevo_uc/src/App.js
+++ yo_te_llevo_uc/src/App.js
@@ -7,6 +7,7 @@ function App() {
         <div className="container rounded border-black bg-white p-3">
           <h1 className="text-3xl font-bold text-red-600 ">
           Hello world!
+          Grupo 23
           </h1>
         </div>
       </header>
