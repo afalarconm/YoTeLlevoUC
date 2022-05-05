484fdfcf06a8a358adf2b84fe3345f1df362c5c4
afalarconm <afalarconm@uc.cl>
2022-05-05 12:02:57 -0400

    added Navbar

--- yo_te_llevo_uc/src/App.js
+++ yo_te_llevo_uc/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
-        <div className="container rounded border-black bg-white p-3">
+        
+        <div className="container">
           <h1 className="text-3xl font-bold text-red-600 ">
           Hello world!
           Grupo 23
