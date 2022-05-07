127d8c688bd92a161c9c78f8c62eca2a72d9cd35
martin-quevedo <martin.quevedo@uc.cl>
2022-05-07 15:47:30 -0400

    Poniendo lo basico y la navbar

---
+++ yo_te_llevo_uc/src/components/LandingPage.js
@@ -0,0 +1,21 @@
+import Navbar from "./Navbar.js";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <header className="App-header">
+        
+        <div className="container">
+          <h1 className="text-3xl font-bold text-red-600 ">
+          Hello world!
+          Grupo 23
+          </h1>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- yo_te_llevo_uc/src/index.js
+++ yo_te_llevo_uc/src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import LandingPage from "./components/LandingPage"
+import LoginPage from "./components/LoginPage"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage/>} />
+      <Route path="/LoginPage" element={<LoginPage/>} />
+
+    </Routes>
+    
+    </BrowserRouter>
   </React.StrictMode>
 );
 
