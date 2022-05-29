fe1ddd4c098aa777028f38773594178b85faa155
afalarconm <afalarconm@uc.cl>
2022-05-29 15:52:37 -0400

    added sequilize db

--- yo_te_llevo_uc/backend/src/routes/api/v1/hello.routes.js
+++ yo_te_llevo_uc/backend/src/routes/api/v1/hello.routes.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const db = require('../../../models');
+
+// const { User } = db; hay que verlo en la pagina de sequilize
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
