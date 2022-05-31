fb408dff26c43df890e1e2601dbcf498160763b7
guillejponce <guillejponce@uc.cl>
2022-05-31 00:53:36 -0400

    Connected viajes create backend

--- yo_te_llevo_uc/backend/src/routes/api/v1/viajes.routes.js
+++ yo_te_llevo_uc/backend/src/routes/api/v1/viajes.routes.js
@@ -1,21 +1,25 @@
-const express = require('express');
-
-const db = require('../../../models');
-
-const { Viajes } = db;
-
+const express = require("express");
+const bcrypt = require("bcrypt");
+const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
+const { Viaje } = require("../../../models");
+dotenv.config();
 
-router.get('/', (req, res) => {
-    res.send({
-        Viajes: {
-            id: 1,
-            nombre: 'Viaje 1',
-            fecha: '2020-01-01',
-            destino: 'Destino 1',
-            hora: '12:00',
-        }
-    });
-    })
-
-    module.exports = router;
+router.post("/CreateViaje/", async (req, res) => {
+    try {     
+            const viaje = await Viaje.create({
+                origen: req.body.origen,
+                destino: req.body.destino,
+                cupos: req.body.cupos,
+                hora_inicio: req.body.hora_inicio,
+                detalles: req.body.comentarios,
+                activo: true,
+          });
+          console.log(viaje); 
+          res.status(201).json(viaje);
+    } catch (e) {
+      res.status(400).json({ error: e });
+    }
+  });
+module.exports = router;
