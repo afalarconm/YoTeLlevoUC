1bd3eedb43652b70b88c9510ac2af0af3c174615
martin-quevedo <martin.quevedo@uc.cl>
2022-05-31 21:11:32 -0400

    añadiendo la tabla de los viajes

--- yo_te_llevo_uc/frontend/src/components/ViajesPage.js
+++ yo_te_llevo_uc/frontend/src/components/ViajesPage.js
@@ -1,7 +1,7 @@
 import Navbar from "./Navbar.js";
 // import {useEffect, useState } from 'react';
 // import { response } from "../../../back/src/utils/app.js";
-import axios from "axios";
+import Axios from "axios";
 import React from "react";
 
 const baseURL = "http://localhost:3001/viajes";
@@ -25,43 +25,35 @@ export default function App() {
     const [post, setPost] = React.useState(null);
 
     React.useEffect(() => {
-        axios.get(baseURL).then((response) => {
+        Axios.get(baseURL).then((response) => {
           setPost(response.data);
         });
       }, []);
 
       if (!post) return null;
 
-    <table class = "table table-stripped">
-        <tr class="bg-info">
-            <th>Origen</th>
-            <th>Destino</th>
-            <th>Cupos</th>
-            <th>Hora Inicio</th>
-            <th>Comentarios</th>
-        </tr>
+   
 
-        <tbody class="myTable">
 
-        </tbody>
 
-    </table>
-
-    function buildTable(post){
-        var table = document.getElementById("myTable")
-
-        for (var i = 0; i < post.length; i++) {
-            var row = <tr>
-                <td>{post[i].origen}</td>
-                <td>{post[i].destino}</td>
-                <td>{post[i].cupos}</td>
-                <td>{post[i].hora_inicio}</td>
-                <td>{post[i].comentarios}</td>
+    const Tabla=post.map(
+        (info)=>{
+            return(
+                <tr>
+                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6" >{info.origen}</td>
+                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{info.destino}</td>
+                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500" >{info.cupos}</td>
+                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500" >{info.hora_inicio}</td>
+                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500" >{info.detalles}</td>
+                <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                            <a href="/" class="text-green-600 hover:text-green-900 px-3">Edit</a>
+                                            <a href="/" class="text-indigo-600 hover:text-indigo-900">Delete</a>
+             </td>
             </tr>
-            table.innerHTML += row
+            )
         }
+    )
 
-    }
 
 
     return (
@@ -69,11 +61,54 @@ export default function App() {
         <Navbar />
 
         <div class="flex-1">
-            <div class="container mx-auto px-4 py-8">
-                <script>
-                    {buildTable(post)}
-                </script>
+    <div class="flex-1">
+     
+        <div class="px-4 sm:px-6 lg:px-8 py-4">
+            <div class="sm:flex sm:items-center">
+                <div class="sm:flex-auto">
+                    <h1 class="text-xl font-semibold text-gray-900">Viajes</h1>
+                    <p class="mt-2 text-sm text-gray-700">El listados de todos los viajes disponibles hasta el momento.</p>
+                </div>
+                <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+                    <button type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Añadir viaje</button>
+                </div>
             </div>
+            <div class="mt-8 flex flex-col">
+                <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                        <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                            <table class="table-fixed min-w-full divide-y divide-gray-300">
+                                <thead class="bg-gray-50">
+                                <tr>
+                                    <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Origen</th>
+                                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Destino</th>
+                                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Cupos</th>
+                                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Hora de Inicio</th>
+                                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Comentarios</th>
+                                    <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                                        <span class="sr-only">Edit</span>
+                                    </th>
+                                </tr>
+                                </thead>
+                                <tbody class="divide-y divide-gray-200 bg-white">
+                                {Tabla}
+                             
+                    
+                                </tbody>
+                                
+                     
+                                
+                          
+                                
+                                
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
       
         <footer class="text-center text-gray-500 text-xs">
          &copy;Tecnologías y Aplicaciones Web - Grupo 23 - 2022.
@@ -81,7 +116,8 @@ export default function App() {
 
 
         </div>
-        </div>
     
     );
 }
+
+
