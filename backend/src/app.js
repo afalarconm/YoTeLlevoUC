537bb9d4d66229c74d698cc0ba090bf7ff3af3b7
afalarconm <afalarconm@uc.cl>
2022-06-23 21:56:20 -0400

    heroku hotfix

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,15 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
+const path = require('path')
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../frontend/components/LandingPage.js'))
+})
 
 const routes = require('./routes/index');
 
