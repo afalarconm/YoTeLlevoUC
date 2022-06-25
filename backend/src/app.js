887728d3663ff4f806efd766472254429cad8421
afalarconm <afalarconm@uc.cl>
2022-06-24 21:15:06 -0400

    prueba n 1004

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,12 +7,12 @@ const app = express();
 const path = require('path')
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'app/frontend/src/index.js')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 console.log('hasta aca por lo menos llegue hrmo')
 
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + 'app/frontend/src/index.js'))});
+  res.sendFile(path.join(__dirname + 'frontend/build/index.html'))});
 
 const routes = require('./routes/index');
 
