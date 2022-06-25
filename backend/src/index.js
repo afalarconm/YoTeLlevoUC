cf29cf283c69afc762c15baef0c0dd5884e291f2
afalarconm <afalarconm@uc.cl>
2022-06-24 20:45:30 -0400

    dnaisundoasd

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,12 +10,12 @@ const PORT = process.env.PORT || 3001;
 const path = require('path')
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../frontend/src/index.js')));
+app.use(express.static(path.join(__dirname, 'app/frontend/src/index.js')));
 console.log('hasta por lo menos llegue hrmo')
 
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../frontend/src/index.js'))});
+  res.sendFile(path.join(__dirname + 'app/frontend/src/index.js'))});
 
 
 db.sequelize.authenticate()
